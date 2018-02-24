02dc88e0348d7ce1c0da95824d886c3705434ebf
Ferran Espigares <github@espigares.es>
2018-02-24 23:58:40 +0100

    Day6 solution

---
+++ day6/day6p1.py
@@ -0,0 +1,20 @@
+fp=open('C:\Users\User\Documents\dev\\adventofcode2017\day6\input')
+input=fp.readline()
+fp.close()
+banks=input.split('\t')
+banks=[int(i) for i in banks]
+#banks=[0,2,7,0]
+len=len(banks)
+states=[]
+count=0
+while banks not in states:
+    count+=1
+    states.append(list(banks))
+    idx=banks.index(max(banks))
+    blocks=banks[idx]
+    banks[idx]=0
+    while blocks>0:
+        idx+=1
+        banks[idx%len]+=1
+        blocks-=1
+print "Redistributions:",count
